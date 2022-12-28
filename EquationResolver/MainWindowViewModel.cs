7431a77be06b795bd58c453039e43270186f5497
Schofield, stuart <546wine@gmail.com>
2022-12-27 21:23:58 -0700

    Advanced where the equation solves itself... still lots to do

--- EquationResolver/MainWindowViewModel.cs
+++ EquationResolver/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,29 +15,7 @@ namespace EquationResolver
     public partial class MainWindowViewModel : ObservableObject
     {
 
-        public MainWindowViewModel()
-        {
-           
-        }
-
-
-
-        //private string originalString = "53 + (27 - 40) * 31 / 69";
-
-        private string originalString = "(6482-5731) + 999 + 000";
-
-        private string stringBeingProcessed;
-
-
-        public string OriginalString
-        {
-            get { return originalString; }
-            set
-            {
-                originalString = value;
-                ASCIIvalues = GetASCIIvalues(originalString);
-            }
-        }
+        #region Private Members
 
         private byte[] ASCIIvalues;
 
@@ -44,6 +23,23 @@ namespace EquationResolver
         {
             return Encoding.ASCII.GetBytes(str);
         }
+
+        #endregion EndRegion-Private Members
+
+        #region Observable Public Properties
+        /// <summary>
+        ///      The original string that is bound to the Equation TextBox
+        /// </summary>
+        [ObservableProperty]
+        private string originalString = "(6482^2) + 999 + 000";
+
+        [ObservableProperty]
+        private string trimmedAndDeSpacedOriginalString;
+
+        #endregion EndRegion:  Observable Public Properties
+
+
+
 
         [ObservableProperty] private Progress progressStatus = Progress.None;
 
@@ -57,24 +53,65 @@ namespace EquationResolver
         public void SolveEquationButtonPressed()
         {
             // Set the string being processed to the original string (only necess. if it never changes)
-            stringBeingProcessed = originalString;
+            trimmedAndDeSpacedOriginalString = OriginalString.Replace(" ", "");
 
-            // Cut out all the blank spaces
-            stringBeingProcessed = stringBeingProcessed.Replace(" ", "");
+            trimmedAndDeSpacedOriginalString = FixForMinus(trimmedAndDeSpacedOriginalString);
 
             // Create the list of ascii values for the string
-            ASCIIvalues = GetASCIIvalues(stringBeingProcessed);
+            ASCIIvalues = GetASCIIvalues(trimmedAndDeSpacedOriginalString);
+
+            // Check to see if any of the characters don't belong in an equation
+            var characterValidationCheck = VerifyAllCharsInStringAreValidEquationCharacters(ASCIIvalues);
+
+            if (!characterValidationCheck) throw new Exception();
+
+
 
             // Notify the Progress that we have pressed the solve button
             ProgressStatus = Progress.SolveButtonHasBeenPressed;
 
             // Call the EquationStringResolver method
-            var equationResult = EquationStringResolver(stringBeingProcessed);
+            var equationResult = EquationStringResolver(trimmedAndDeSpacedOriginalString);
 
-            
+
 
             Result = equationResult;
         }
+
+        private string FixForMinus(string trimmedAndDeSpacedOriginalString)
+        {
+            var bytes = GetASCIIvalues(trimmedAndDeSpacedOriginalString);
+
+            string result = "";
+
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                if (bytes[i] == 45)
+                {
+                    if (i == 0) result = result + trimmedAndDeSpacedOriginalString.Substring(i, 1);
+                    else if (IsANumber(bytes[i - 1])) result = result + "+-";
+                    else result = result + "-";
+                }
+                 
+                else result = result + trimmedAndDeSpacedOriginalString.Substring(i, 1);
+            }
+
+            return result;
+        }
+
+        private bool VerifyAllCharsInStringAreValidEquationCharacters(byte[] bytesToCheck)
+        {
+            foreach (var b in bytesToCheck)
+            {
+
+                if (!((b >= 40 && b <= 43) || (b >= 45 && b <= 57) || (b == 94))) return false;
+            }
+
+            return true;
+        }
+
+
+
 
         private double EquationStringResolver(string stringToResolve)
         {
@@ -88,9 +125,9 @@ namespace EquationResolver
                 var t = bytes[i];
 
                 // If the character is not 0-9  +  -  /  *  ^  ( or ) fail the equation
-                if( !( (bytes[i] < 40) ||  (bytes[i] > 57 && bytes[i] < 94)  || (bytes[i] > 94) || bytes[i] != 46 || bytes[i] != 47))
+                if (!((bytes[i] < 40) || (bytes[i] > 57 && bytes[i] < 94) || (bytes[i] > 94) || bytes[i] != 46 || bytes[i] != 47))
                 {
-            
+
                     FailEquation("invalid character in equation");
 
                     throw new Exception();
@@ -103,7 +140,7 @@ namespace EquationResolver
                     var result = CutOutTextInsideOfBrackets(i, stringToResolve);
 
                     // Remember the first part of the string before the opening bracket
-                    var firstHalfOfString = stringToResolve.Substring(0,i);
+                    var firstHalfOfString = stringToResolve.Substring(0, i);
 
                     // Remember the last part of the string after the corresponding closing bracket...)  
                     var secondHalfOfString = stringToResolve.Substring(i + (result.Length + 2));
@@ -112,155 +149,209 @@ namespace EquationResolver
                 }
 
 
-                
+
             }
 
             return SolveSimlifiedEquation(stringToResolve);
 
-            
+
         }
 
         private double SolveSimlifiedEquation(string stringToResolve)
         {
             var stringrecieved = stringToResolve;
 
-            // List of operators asc  ^  *   /   +   -
-            int[] operatorsAscii = { 94, 42, 47, 43, 45 };
+            // List of operators asc  ^  *   /   +   
+            int[] operatorsAscii = { 94, 42, 47, 43 };
 
-            foreach(var o in operatorsAscii)
+            foreach (var o in operatorsAscii)
             {
-                var x = o;
+                int i = 0;
 
-                for (int i = 0; i < stringrecieved.Length; i++)
+                var l = stringrecieved.Length;
+
+                var r = "";
+                var firstintstr = "";
+                var secondintstr = "";
+                bool operatorFound = false;
+                while (i <= l)
                 {
-                    var y = stringrecieved[i];
-
-                    if (o == (int)stringrecieved[i]) 
+                    int a = 0;
+                    if (i < l)
                     {
-                        var numberBeforeStartPosition = FindNumberBeforeOperatorAsString(stringrecieved, i);
-                        var numberAfterEndPosition = FindNumberAfterOperatorAsString(stringrecieved, i + 1);
+                        a = (int)stringrecieved[i];
+                        var y = stringrecieved[i];
 
-                        
-                        //var numberBeforeOperatorString = stringToResolve.Substring(numberBeforeStartPosition, i - 1 - numberBeforeStartPosition);
                     }
+
+
+
+                    if (((a == 45) || (a == 46) || (a >= 48 && a <= 57)) && !operatorFound)
+                    {
+                        firstintstr = firstintstr + stringrecieved.Substring(i, 1);
+                        
+                    }
+                    else if ((a == 45) || (a == 46) || ((a >= 48 && a <= 57)) && operatorFound)
+                    {
+                        if (!(secondintstr.Length > 0)) { firstintstr = firstintstr.Substring(0, firstintstr.Length - 1); }
+                        secondintstr = secondintstr + stringrecieved.Substring(i , 1);
+                        
+                    }
+                    else if ((a == o) && !operatorFound)
+                    {
+                        operatorFound = true;
+                        
+                    }
+
+
+                    else if (operatorFound)
+                    {
+                        double sol;
+
+                        if (o == 94) sol = Math.Pow(Convert.ToDouble(firstintstr), Convert.ToDouble(secondintstr));
+
+                        if (o == 42) sol = Convert.ToDouble(firstintstr) * Convert.ToDouble(secondintstr);
+
+                        if (o == 47) sol = Convert.ToDouble(firstintstr) / Convert.ToDouble(secondintstr);
+
+                        if (o == 43) sol = Convert.ToDouble(firstintstr) + Convert.ToDouble(secondintstr);
+                    }
+                    else
+                    {
+                        firstintstr = secondintstr;
+                        secondintstr = "";
+                    }
+                    i++;
                 }
+
             }
 
             return 0;
+
         }
 
-        private string FindNumberBeforeOperatorAsString(string stringRecieved, int index)
+
+
+
+    
+
+
+
+    private string FindNumberBeforeOperatorAsString(string stringRecieved, int index)
+    {
+
+
+        var firstHalfOfString = stringRecieved.Substring(0, index);
+
+        var i = index;
+
+        foreach (var c in stringRecieved.Substring(0, index).Reverse())
+        {
+            if (!IsANumber(c))
+            {
+                var x = stringRecieved.Substring(i, index - i + 1);
+
+                return x;
+            }
+            i--;
+        }
+
+        var result = stringRecieved.Substring(0, index);
+
+        return result;
+
+    }
+
+
+
+    private string FindNumberAfterOperatorAsString(string stringRecieved, int index)
+    {
+
+
+        var secondHalfOfString = stringRecieved.Substring(index);
+
+        var i = index;
+
+        foreach (var c in secondHalfOfString)
+        {
+            if (!IsANumber(c))
+            {
+                var x = stringRecieved.Substring(i, index - i + 1);
+
+                return x;
+            }
+            i--;
+        }
+
+        var result = secondHalfOfString;
+
+        return result;
+
+    }
+
+
+
+
+
+    private int FindEndPositionOfNumberAfterOperator(string stringrecieved, int index)
+    {
+        var i = index;
+
+        foreach (var c in stringrecieved.Substring(index))
+        {
+            if (!IsANumber(c)) return i;
+            i--;
+        }
+        return stringrecieved.Length;
+
+    }
+
+
+
+
+    private bool IsANumber(int charAscii)
+    {
+        if (((charAscii < 48) || (charAscii > 57)) && charAscii != 46) return false;
+        else return true;
+    }
+
+    private string CutOutTextInsideOfBrackets(int openBracketPosition, string stringToCutBracketsOutOf)
+    {
+        string result = null;
+        int numberOfOpenBrackets = 0;
+
+        var bytes = GetASCIIvalues(stringToCutBracketsOutOf);
+
+        for (int i = openBracketPosition + 1; i < stringToCutBracketsOutOf.Length; i++)
         {
 
-
-            var firstHalfOfString = stringRecieved.Substring(0,index);
-
-            var i = index;
-
-            foreach( var c in stringRecieved.Substring(0,index).Reverse())
+            if (bytes[i] == 40) numberOfOpenBrackets++;
+            if (bytes[i] == 41)
             {
-                if (!IsANumber(c))
+
+                if (numberOfOpenBrackets == 0)
                 {
-                  var x = stringRecieved.Substring(i, index-i+1);
-
-                    return x;
+                    result = stringToCutBracketsOutOf.Substring(openBracketPosition + 1, i - openBracketPosition - 1);
                 }
-                i--;
+                numberOfOpenBrackets--;
             }
 
-            var result = stringRecieved.Substring(0, index);
-
-            return result;
-
+            if (result != null) break;
         }
 
-
-
-        private string FindNumberAfterOperatorAsString(string stringRecieved, int index)
+        if (result != null) { return result; }
+        else
         {
-
-
-            var secondHalfOfString = stringRecieved.Substring(index);
-
-            var i = index;
-
-            foreach (var c in secondHalfOfString)
-            {
-                if (!IsANumber(c))
-                {
-                    var x = stringRecieved.Substring(i, index - i + 1);
-
-                    return x;
-                }
-                i--;
-            }
-
-            var result = secondHalfOfString;
-
-            return result;
-
-        }
-
-
-
-
-
-        private int FindEndPositionOfNumberAfterOperator(string stringrecieved, int index)
-        {
-            var i = index;
-
-            foreach (var c in stringrecieved.Substring(index))
-            {
-                if (!IsANumber(c)) return i;
-                i--;
-            }
-            return stringrecieved.Length;
-
-        }
-
-        private bool IsANumber (int charAscii)
-        {
-            if (((charAscii < 48) || (charAscii > 57)) && charAscii != 46) return false;
-            else return true;
-        }
-
-        private string CutOutTextInsideOfBrackets(int openBracketPosition, string stringToCutBracketsOutOf)
-        {
-            string result = null;
-            int numberOfOpenBrackets = 0;
-
-            var bytes = GetASCIIvalues(stringToCutBracketsOutOf);
-            
-            for (int i=openBracketPosition+1; i< stringToCutBracketsOutOf.Length; i++)
-            {
-                
-                if (bytes[i] == 40) numberOfOpenBrackets++;
-                if (bytes[i] == 41)
-                {
-                    
-                    if (numberOfOpenBrackets== 0)
-                    {
-                        result = stringToCutBracketsOutOf.Substring(openBracketPosition + 1, i- openBracketPosition - 1);
-                    }
-                    numberOfOpenBrackets--;
-                }
-
-                if (result != null) break;
-            }
-
-            if (result != null) { return result; }
-            else
-            {
-                throw new Exception();
-                return string.Empty;
-            }
-        }
-
-        private void FailEquation(string message)
-        {
-            EquationFailed = true;
-
-            ErrorMessage = message;
+            throw new Exception();
+            return string.Empty;
         }
     }
+
+    private void FailEquation(string message)
+    {
+        EquationFailed = true;
+
+        ErrorMessage = message;
+    }
+}
 }
